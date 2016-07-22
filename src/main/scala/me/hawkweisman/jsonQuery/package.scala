74c4827a71f84fda9d39eb12214ec652d9f14c75
Hawk Weisman <hi@hawkweisman.me>
2016-07-22 10:10:09 -0400

    At least it works for now

--- src/main/scala/me/hawkweisman/jsonQuery/package.scala
+++ src/main/scala/me/hawkweisman/jsonQuery/package.scala
@@ -30,29 +30,42 @@ extends UnboxedUnion {
       String)
 
   trait Queryable extends UnboxedUnion {
-    protected[this] def rawOption: Option[Object]
+//    protected[this] def rawOption: Option[Object]
 
     @inline final def \ (key: String): Query = new Query(key, this)
 
-    @inline final def as[T: FromJson#λ : ClassTag]: Option[T]
-    = rawOption flatMap {
-      case it: T => Some(it)
-      case _ => None
-    }
+    def asJsonObject: Option[JSONObject]
+//    @inline final def as[T: FromJson#λ : ClassTag]: Option[T]
+//    = rawOption flatMap {
+//      case it: T => Some(it)
+//      case _ => None
+//    }
   }
 
   class Query(val key: String, private[this] val parent: Queryable)
   extends Queryable with UnboxedUnion {
-    override protected[this] lazy val rawOption
-      = parent.as[JSONObject] flatMap { parentObj: JSONObject =>
-          Option(parentObj opt key)
-      }
+    //    override protected[this] lazy val rawOption
+//      = parent.as[JSONObject] flatMap { parentObj: JSONObject =>
+//          Option(parentObj opt key)
+//      }
 //      = ???
+    @inline private[this] def as[T](f: JSONObject => T): Option[T]
+      = parent.asJsonObject flatMap { p => Option(f(p)) }
+
+    @inline override def asJsonObject: Option[JSONObject]
+      = as(_ optJSONObject key)
+    
+    @inline def asString: Option[String] = as(_ optString key)
+    @inline def asDouble: Option[Double] = as(_ optDouble key)
+    @inline def asInt: Option[Int] = as(_ optInt key)
+    @inline def asJsonArray: Option[JSONArray] = as(_ optJSONArray key)
   }
 
   implicit class JsonQueryOps(val obj: JSONObject)
   extends Queryable {
-    override protected[this] lazy val rawOption = Option(obj)
+    //    override protected[this] lazy val rawOption = Option(obj)
+    @inline override def asJsonObject: Option[JSONObject]
+      = Option(obj)
   }
 
 }
