0f51617d461cf3025080e2d072770d29d2574d90
Hawk Weisman <hi@hawkweisman.me>
2016-07-23 12:13:05 -0400

    fix incorrect extractor

--- src/main/scala/me/hawkweisman/jsonQuery/extractors.scala
+++ src/main/scala/me/hawkweisman/jsonQuery/extractors.scala
@@ -1,13 +1,17 @@
 package me.hawkweisman.jsonQuery
 
-import me.hawkweisman.jsonQuery.queries.{Query, Queryable}
+import me.hawkweisman.jsonQuery.queries._
+import org.json.{JSONArray, JSONObject}
+
+import scala.language.postfixOps
+import scala.reflect.ClassTag
 
 /**
   * Created by Eliza on 7/23/16.
   */
 object extractors {
 
-  sealed class Json[T: FromJson#Element] {
+  sealed class Json[T: FromJson#Element : ClassTag] {
     @inline def unapply(query: Queryable): Option[T]
       = query.asOption[T]
   }
@@ -16,5 +20,15 @@ object extractors {
   object JsonDouble extends Json[Double]
   object JsonBool extends Json[Boolean]
   object JsonString extends Json[String]
+
+  object JsonArray extends Json[JSONArray] {
+    @inline def unapplySeq(query: Queryable): Option[Seq[Index]]
+      = query.asOption[JSONArray] map { _ toSeq }
+  }
+
+  object JsonObject extends Json[JSONObject] {
+    @inline def unapplySeq(query: Queryable): Option[Seq[(String, Query)]]
+      = ???
+  }
 
 }
