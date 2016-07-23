470662cf6fdd17dc96f157c600f96c804e9a4757
Hawk Weisman <hi@hawkweisman.me>
2016-07-23 11:32:22 -0400

    fix indentation

--- src/main/scala/me/hawkweisman/jsonQuery/queries.scala
+++ src/main/scala/me/hawkweisman/jsonQuery/queries.scala
@@ -8,46 +8,47 @@ import scala.util.{Failure, Success, Try}
 /**
   * Created by Eliza on 7/23/16.
   */
-object queries extends UnboxedUnion {
-  trait Queryable
-    extends UnboxedUnion {
+object queries
+extends UnboxedUnion {
+
+  trait Queryable {
 
     protected[this] def rawOption: Option[Object]
     protected[this] def rawTry: Try[Object]
 
     @inline final def as[T: FromJson#Element : ClassTag]: Try[T]
-    = rawTry flatMap {
-      case it: T => Success(it)
-      case it => Failure(
-        // TODO: make this error better
-        new Exception(s"Could not represent $it as requested type"))
-    }
+      = rawTry flatMap {
+        case it: T => Success(it)
+        case it => Failure(
+          // TODO: make this error better
+          new Exception(s"Could not represent $it as requested type"))
+      }
 
     @inline final def asOption[T: FromJson#Element : ClassTag]: Option[T]
-    = rawOption flatMap {
-      case it: T => Some(it)
-      case _ => None
-    }
+      = rawOption flatMap {
+        case it: T => Some(it)
+        case _ => None
+      }
 
     @inline def \ (key: String): Queryable = new Query(key, this)
   }
 
   class Query(val key: String, private[this] val parent: Queryable)
-    extends Queryable {
+  extends Queryable {
 
     override protected[this] lazy val rawOption
-    = parent.asOption[JSONObject] flatMap { parentObj: JSONObject =>
-      Option(parentObj opt key)
-    }
+      = parent.asOption[JSONObject] flatMap { parentObj: JSONObject =>
+        Option(parentObj opt key)
+      }
 
     override protected[this] lazy val rawTry
-    = parent.as[JSONObject] flatMap { parentObj: JSONObject =>
-      Try(parentObj get key)
-    }
+      = parent.as[JSONObject] flatMap { parentObj: JSONObject =>
+        Try(parentObj get key)
+      }
   }
 
   class Index(val idx: Int, private[this] val array: JSONArray)
-    extends Queryable {
+  extends Queryable {
 
     override protected[this] lazy val rawOption
     = Option(array opt idx)
@@ -57,13 +58,13 @@ object queries extends UnboxedUnion {
   }
 
   implicit class QueryableJsonObject(val obj: JSONObject)
-    extends Queryable {
+  extends Queryable {
     override protected[this] lazy val rawOption = Option(obj)
     override protected[this] lazy val rawTry = Success(obj)
   }
 
   implicit class IndexableJsonArray(val array: JSONArray)
-    extends Traversable[Queryable] {
+  extends Traversable[Queryable] {
     lazy val length = array.length
 
     /** Attempt to index the JSON array.
@@ -75,17 +76,17 @@ object queries extends UnboxedUnion {
       * @throws    ArrayIndexOutOfBoundsException if `i < 0` or `length <= i`
       */
     @inline def apply(idx: Int): Index
-    = idx match {
-      case _ if idx < 0 =>
-        throw new ArrayIndexOutOfBoundsException(s"index $idx < 0")
-      case _ if length <= idx =>
-        throw new ArrayIndexOutOfBoundsException(
-          s"index $idx >= length ($length)")
-      case _ => new Index(idx, array)
-    }
+      = idx match {
+        case _ if idx < 0 =>
+          throw new ArrayIndexOutOfBoundsException(s"index $idx < 0")
+        case _ if length <= idx =>
+          throw new ArrayIndexOutOfBoundsException(
+            s"index $idx >= length ($length)")
+        case _ => new Index(idx, array)
+      }
 
     override def foreach[U](f: (Queryable) => U): Unit
-    = for { i <- 0 until length } f(new Index(i, array))
+      = for { i <- 0 until length } f(new Index(i, array))
   }
 
 }
